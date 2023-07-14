937dae8f1a72b0b8166d6535a7224ca86af3e844
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-07-14 17:50:21 +0300

    add exception for the case if there is no book

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ url = "https://tululu.org/txt.php"
 
 os.makedirs('books', exist_ok=True)
 
-for i in range(1, 10):
-    params = {
-        "id": i,
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
 
-    filename = f'id{i}.txt'
-    with open(f'books/{filename}', 'wb') as file:
-        file.write(response.content)
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
+
+if __name__ == '__main__':
+    for book_id in range(1, 10):
+        params = {
+            "id": book_id,
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            continue
+
+        filename = f'id{book_id}.txt'
+        with open(f'books/{filename}', 'wb') as file:
+            file.write(response.content)
