1e46bb07b2e93ea1e4a689e43f5ea8d8598664d0
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-07-26 23:07:37 +0300

    add logging messages about errors

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import time
-
+import sys
 import requests
 import argparse
+import logging
 
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
@@ -18,12 +19,11 @@ def safe_get(url, params=None):
     while True:
         try:
             response = requests.get(url, params)
-            print(response.status_code)
             response.raise_for_status()
             if response.status_code == 200:
                 return response
-        except requests.ConnectionError:
-            print('Ошибка соединения')
+        except requests.ConnectionError as e:
+            logging.error('Ошибка соединения' + str(e))
             time.sleep(5)
 
 
@@ -41,14 +41,15 @@ def download_txt(url, filename, book_id, folder='books/'):
 
     try:
         response = safe_get(url, params)
-    except requests.HTTPError:
-        print(f'HTTPError для ссылки {url}')
+    except requests.HTTPError as e:
+        logging.error(f'HTTPError для ссылки {url}' + str(e))
         return
 
     try:
         check_for_redirect(response)
-    except requests.HTTPError:
-        return
+    except requests.HTTPError as e:
+        logging.error(f'Текст книги не найден. id: {book_id}\n' + str(e))
+        sys.stderr.write(str(e))
 
     filename = sanitize_filename(filename)
     path = os.path.join(folder, filename)
@@ -67,14 +68,15 @@ def download_image(url, filename, folder='imgs/'):
     """
     try:
         response = safe_get(url)
-    except requests.HTTPError:
-        print(f'HTTPError для ссылки {url}')
+    except requests.HTTPError as e:
+        logging.error(f'HTTPError для ссылки {url}' + str(e))
         return
 
     try:
         check_for_redirect(response)
-    except requests.HTTPError:
-        return
+    except requests.HTTPError as e:
+        logging.error(f'Изображение не найдено\n' + str(e))
+        sys.stderr.write(str(e))
 
     filename = sanitize_filename(filename)
     path = os.path.join(folder, filename)
@@ -128,13 +130,15 @@ def main(start_id, end_id):
 
         try:
             response = safe_get(book_page_url)
-        except requests.HTTPError:
-            print(f'HTTPError для ссылки {book_page_url}')
+        except requests.HTTPError as e:
+            logging.error(f'HTTPError для ссылки {book_page_url}\n {str(e)}')
             continue
 
         try:
             check_for_redirect(response)
-        except requests.HTTPError:
+        except requests.HTTPError as e:
+            logging.error(f'Книга не найдена. id: {book_id}\n {str(e)}')
+            sys.stderr.write(str(e))
             continue
 
         book = parse_book_page(response.text)
