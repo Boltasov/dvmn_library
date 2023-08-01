9ff01a07824dd66dece23354ad22eb762cc4733a
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-08-01 05:24:32 +0300

    add setting start_page and end_page with arguments

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,21 +2,22 @@ import os
 import json
 import requests
 import logging
-import pickle
+import argparse
 
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from main import parse_book_page, download_txt, download_image
 
 
-def parse_book_urls():
+def parse_book_urls(start_page, end_page):
     science_fiction_url = 'https://tululu.org/l55/'
     base_url = 'https://tululu.org/'
 
     # book_id_urls = []
     book_urls = []
-    for page in range(1, 2):
+    for page in range(start_page, end_page+1):
         page_url = urljoin(science_fiction_url, f'{page}/')
+        print('Страница:', page_url)
 
         response = requests.get(page_url)
         response.raise_for_status()
@@ -38,8 +39,16 @@ def parse_book_urls():
     return book_urls
 
 
-if __name__ == '__main__':
-    book_urls = parse_book_urls()
+def main():
+    parser = argparse.ArgumentParser(
+        prog='BookParser',
+        description='Скрипт загружает книги с указанных страниц с сайта tululu.org',
+    )
+    parser.add_argument("--start_page", help='Номер страницы, с которой начнём парсить', type=int, required=True)
+    parser.add_argument("--end_page", help='Номер страницы, на которой закончим парсить', type=int, default=701)
+    args = parser.parse_args()
+
+    book_urls = parse_book_urls(args.start_page, args.end_page)
     base_url = 'https://tululu.org/'
 
     os.makedirs('books', exist_ok=True)
@@ -50,6 +59,7 @@ if __name__ == '__main__':
     for book_url in book_urls:
         response = requests.get(book_url)
         response.raise_for_status()
+        print(book_url)
 
         book = parse_book_page(response.text)
 
@@ -69,9 +79,13 @@ if __name__ == '__main__':
 
             books.append(book)
 
-            print(f'{book["title"]}')
+            #print(f'{book["title"]}')
 
     books_json = json.dumps(books, ensure_ascii=False, indent=4)
     with open('books.txt', 'w') as books_file:
         books_file.write(books_json)
         print('Done writing list into a file')
+
+
+if __name__ == '__main__':
+    main()
